741e083a8e63c0b5619b86dcc2f129917856dbd8
Furkan <36599886+raksix@users.noreply.github.com>
2024-12-17 14:44:06 +0300

    user register request

--- myapp/firstapp/views.py
+++ myapp/firstapp/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 from . import mongo
-
+import json
+import datetime
 
 # Create your views here.
 def index(request):
@@ -10,10 +11,43 @@ def index(request):
 
 def register(request):
     if request.method == "POST":
+        body = json.loads(request.body)
+
+        if not body["username"] or not body["password"] or not body["email"]:
+            return JsonResponse(
+                {
+                    "error": True,
+                    "message": "Please provide all the required fields",
+                }
+            )
+        
+        exists_user = mongo.users.find_one({
+            "$or": [
+                {"username": body["username"]},
+                {"email": body["email"]}
+            ]
+        })
+
+        if exists_user:
+            return JsonResponse(
+                {
+                    "error": True,
+                    "message": "User already exists",
+                }
+            )
+            
+
+        mongo.users.insert_one({
+            "username": body["username"],
+            "password": body["password"],
+            "email": body["email"],
+            "data": datetime.datetime.now()
+        })
+
         return JsonResponse(
             {
                 "error": False,
-                "message": "Only POST method is allowed for this endpoint",
+                "message": "User registered successfully",
             }
         )
     else:
@@ -26,9 +60,6 @@ def register(request):
 
 
 def duyurular(request):
-
-    print(request.method)
-
     return JsonResponse(
         {
             "duyurular": [
